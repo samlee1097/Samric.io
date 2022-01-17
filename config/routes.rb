7ccc9eaacf70509727cd934187b014dbbd999acf
Eric Yi <erickanginyi@gmail.com>
2022-01-17 01:27:08 -0500

    Mount initial ActionCable server action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+   mount ActionCable.server => "/cable"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
