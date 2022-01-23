dac18209a901348360aab8df4d14035c6e4455f9
Samuel <samlee1097@yahoo.com>
2022-01-23 16:51:36 -0500

    Create channel subscription upon loading game room

--- client/src/Components/GameRoom/Chatbox/ChatBox.js
+++ client/src/Components/GameRoom/Chatbox/ChatBox.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 function ChatBox() {
+
+    const cable = useContext(ActionCableContext)
+    const [channel, setChannel] = useState(null)
+    const { gameroomId } = useParams()
+
+    useEffect(() => {
+        const channel = cable.subscriptions.create({
+          channel: 'MessagesChannel',
+          id: gameroomId,
+        })
+    
+        setChannel(channel)
+    
+        return () => {
+          channel.unsubscribe()
+        }
+      }, [gameroomId])
+
     return (
        <div id="gameroom-chatbox">
            <p>ChatBox</p>
