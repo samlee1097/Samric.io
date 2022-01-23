07f56548f4e2b3da90e3bc9a4c4d99a4e0af196e
Samuel <samlee1097@yahoo.com>
2022-01-23 16:55:19 -0500

    Create channel subscription upon loading game room

--- client/src/Components/GameRoom/Chatbox/ChatBox.js
+++ client/src/Components/GameRoom/Chatbox/ChatBox.js
@@ -4,14 +4,17 @@ function ChatBox() {
 
     const cable = useContext(ActionCableContext)
     const [channel, setChannel] = useState(null)
+    // React Hook method that extracts an object of the key/value pairs of the URL parameter
     const { gameroomId } = useParams()
 
     useEffect(() => {
+        // Create a new subscription w/ channel & id, corresponding to the subscribed action in our backend
         const channel = cable.subscriptions.create({
           channel: 'MessagesChannel',
           id: gameroomId,
         })
-    
+        
+        // Update channel state in order to use subscription methods
         setChannel(channel)
     
         return () => {
