4eae750d77475c3df2b6990babe1f3a593db37a9
Samuel <samlee1097@yahoo.com>
2022-02-16 03:22:25 -0500

    test

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://sketch-io.vercel.app/",
-    methods: ["GET", "POST"],
+    origin: "*",
   },
 });
 
